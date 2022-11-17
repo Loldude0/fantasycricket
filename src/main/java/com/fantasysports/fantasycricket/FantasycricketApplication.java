c03ed414b9e820c236dcf4e3a7e4278b4aa91875
Loldude0 <aryan.tajne@gmail.com>
2022-11-17 00:27:34 -0600

    first commit

---
+++ src/main/java/com/fantasysports/fantasycricket/FantasycricketApplication.java
@@ -0,0 +1,13 @@
+package com.fantasysports.fantasycricket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasycricketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantasycricketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fantasysports/fantasycricket/FantasycricketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fantasysports.fantasycricket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantasycricketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
